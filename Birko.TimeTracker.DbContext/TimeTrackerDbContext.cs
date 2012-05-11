dbbb46dc6f4e2054b03ae11c9c030b5729b41327
unknown <Frantiek@Oravadragon.(none)>
2012-05-11 07:33:33 +0200

    Project Init

---
+++ Birko.TimeTracker.DbContext/TimeTrackerDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Birko.TimeTracker.Entities;
+
+namespace Birko.TimeTracker.DbContext
+{
+    public class TimeTrackerDbContext : System.Data.Entity.DbContext
+    {
+        public DbSet<Birko.TimeTracker.Entities.Category> Categories { get; set; }
+        public DbSet<Birko.TimeTracker.Entities.Task> Tasks { get; set; }
+        public DbSet<Birko.TimeTracker.Entities.Tag> Tags { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Birko.TimeTracker.Entities.Tag>().Property(e => e.Name).IsRequired();
+            modelBuilder.Entity<Birko.TimeTracker.Entities.Task>().Property(e => e.Name).IsRequired();
+            modelBuilder.Entity<Birko.TimeTracker.Entities.Category>().Property(e => e.Name).IsRequired();
+            modelBuilder.Entity<Birko.TimeTracker.Entities.Category>().Property(e => e.Group).IsOptional();
+            modelBuilder.Entity<Birko.TimeTracker.Entities.Category>().HasMany<Birko.TimeTracker.Entities.Task>(e => e.Tasks);
+            modelBuilder.Entity<Birko.TimeTracker.Entities.Tag>().HasMany<Birko.TimeTracker.Entities.Task>(e => e.Tasks);
+            modelBuilder.Entity<Birko.TimeTracker.Entities.Task>().HasMany<Birko.TimeTracker.Entities.Tag>(e => e.Tags);
+        }
+    }
+}
---
+++ Birko.TimeTracker.Entities/Task.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Birko.TimeTracker.Entities
+{
+    public class Task
+    {
+        [Key]
+        [DatabaseGeneratedAttribute(System.ComponentModel.DataAnnotations.Schema.DatabaseGeneratedOption.None)]
+        public Guid ID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+        [ForeignKey("Category")]
+        public Guid CategoryID { get; set; }
+        public Category Category { get; set; }
+        public ICollection<Tag> Tags { get; set; }
+        [NotMappedAttribute]
+        public TimeSpan Duration 
+        {
+            get 
+            {
+                return this.GetDuration();
+            }
+        }
+
+        private TimeSpan GetDuration()
+        {
+            if (Start != null && End != null)
+            {
+                return End - Start;
+            }
+            else if (Start != null && End == null)
+            {
+                return DateTime.Now - Start;
+            }
+            else 
+            {
+                return new TimeSpan(0);
+            }
+        }
+    }
+}
